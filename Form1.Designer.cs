45a8fd3f66e88d1208eaa566abb20fa1fa69fc21
Bartash Yriy <66261160+uyriy18@users.noreply.github.com>
2020-12-02 19:52:40 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace DataAdapter_ADO_NET
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbx_Select = new System.Windows.Forms.TextBox();
+            this.btn_Fill = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tab = new System.Windows.Forms.TabControl();
+            this.SuspendLayout();
+            // 
+            // txbx_Select
+            // 
+            this.txbx_Select.Location = new System.Drawing.Point(99, 329);
+            this.txbx_Select.Name = "txbx_Select";
+            this.txbx_Select.Size = new System.Drawing.Size(440, 20);
+            this.txbx_Select.TabIndex = 0;
+            // 
+            // btn_Fill
+            // 
+            this.btn_Fill.Location = new System.Drawing.Point(18, 329);
+            this.btn_Fill.Name = "btn_Fill";
+            this.btn_Fill.Size = new System.Drawing.Size(75, 23);
+            this.btn_Fill.TabIndex = 2;
+            this.btn_Fill.Text = "Fill table";
+            this.btn_Fill.UseVisualStyleBackColor = true;
+            this.btn_Fill.Click += new System.EventHandler(this.btn_Fill_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.DisplayIndex = 0;
+            this.columnHeader1.Text = "Title";
+            this.columnHeader1.Width = 326;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.DisplayIndex = 1;
+            this.columnHeader2.Text = "Price";
+            this.columnHeader2.Width = 74;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.DisplayIndex = 2;
+            this.columnHeader3.Text = "Pages";
+            this.columnHeader3.Width = 78;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.DisplayIndex = 3;
+            this.columnHeader4.Text = "AuthorId";
+            this.columnHeader4.Width = 362;
+            // 
+            // tab
+            // 
+            this.tab.Location = new System.Drawing.Point(18, 13);
+            this.tab.Name = "tab";
+            this.tab.SelectedIndex = 0;
+            this.tab.Size = new System.Drawing.Size(521, 310);
+            this.tab.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.tab);
+            this.Controls.Add(this.btn_Fill);
+            this.Controls.Add(this.txbx_Select);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbx_Select;
+        private System.Windows.Forms.Button btn_Fill;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.TabControl tab;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Diagnostics;
+
+namespace DataAdapter_ADO_NET
+{
+    public partial class Form1 : Form
+    {
+        SqlDataAdapter adapter ;
+        SqlCommandBuilder cmd;
+        DataSet set;
+        SqlConnection connection;
+        ListView listView1;
+        static int i = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            tab.Visible = false;
+            
+        }
+
+        private void btn_Fill_Click(object sender, EventArgs e)
+        {
+            listView1 = new ListView();
+            tab.Visible = true;
+            TabPage page = new TabPage();
+            tab.TabPages.Add(page.Text = "Tab " + i.ToString());
+            i++;
+            page.Select();
+            page.Controls.Add(listView1);
+            listView1.Items.Clear();
+            connection = new SqlConnection(ConfigurationManager.ConnectionStrings["MyConn"].ConnectionString);
+            SqlCommand command = new SqlCommand(txbx_Select.Text, connection);
+            try
+            {
+
+                adapter = new SqlDataAdapter(command);
+                set = new DataSet();
+                adapter.Fill(set);
+                foreach (DataRow ds in set.Tables[0].Rows)
+                {
+                    ListViewItem item = new ListViewItem(ds["Title"].ToString());
+                    item.SubItems.Add(ds["Price"].ToString());
+                    item.SubItems.Add(ds["Pages"].ToString());
+                    item.SubItems.Add(ds["AuthorId"].ToString());
+                    listView1.Items.Add(item);
+                }
+              
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+        }
+
+    
+    }
+}
+//connection.Open();
+//reader = command.ExecuteReader();
+//while (reader.Read())
+//{
+//    ListViewItem item = new ListViewItem(reader["Title"].ToString());
+//    item.SubItems.Add(reader["Price"].ToString());
+//    item.SubItems.Add(reader["Pages"].ToString());
+//    item.SubItems.Add(reader["AuthorId"].ToString());
+
+//    listView1.Items.Add(item);
+//}
+
+
+
+//DataTable table = new DataTable();
+//adapter.Fill(table);
+//foreach(DataRow dr in table.Rows)
+//{
+//    ListViewItem item = new ListViewItem(dr["Title"].ToString());
+//    item.SubItems.Add(dr["Price"].ToString());
+//    item.SubItems.Add(dr["Pages"].ToString());
+//    item.SubItems.Add(dr["AuthorId"].ToString());
+
+//    listView1.Items.Add(item);
+//}
+
+
+
+//DataTable table = new DataTable();
+//adapter.Fill(table);
+//for(int i = 0; i < table.Rows.Count; i++)
+//{
+//    ListViewItem item = new ListViewItem(table.Rows[i]["Title"].ToString());
+//    item.SubItems.Add(table.Rows[i]["Price"].ToString());
+//    item.SubItems.Add(table.Rows[i]["Pages"].ToString());
+//    item.SubItems.Add(table.Rows[i]["AuthorId"].ToString());
+
+//    listView1.Items.Add(item);
+//}
+
